b68b3035f7b415520f195b27429378f504f3c85d
LucHa2000 <66839444+LucHa2000@users.noreply.github.com>
2021-09-04 09:18:59 +0700

    commit Luc_Branch

---
+++ Luc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTbac1_2
+{
+    public partial class PT1 : Form
+    {
+        public PT1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PT1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private String Process(double a, double b)
+
+        {
+
+            String result = "";
+            if (a == 0)
+            {
+                if (b == 0)
+                {
+                    result = "phuong trinh vo so nghiem";
+                }
+                else
+                {
+                    result = "phuong trinh vo nghiem";
+                }
+            }
+            else
+            {
+                result= -b/a + "";
+
+            }
+            return result;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double a = Convert.ToDouble(A.Text);
+            Double b = Convert.ToDouble(B.Text);
+            String c = Process(a, b);
+            RS.Text = c;
+       
+
+
+        }
+    }
+}
