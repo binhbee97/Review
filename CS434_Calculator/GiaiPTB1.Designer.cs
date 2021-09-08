f461f70fa4720ec82c89dfcd9f5dfd0e859a8f5a
Binh <binhbee97@gmail.com>
2021-09-08 11:19:59 +0700

    Upcode

---
+++ CS434_Calculator/GiaiPTB1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace CS434_Calculator
+{
+    partial class GiaiPTB1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.RS = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.A = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(92, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 45);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Giải";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // RS
+            // 
+            this.RS.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RS.Location = new System.Drawing.Point(189, 310);
+            this.RS.Name = "RS";
+            this.RS.Size = new System.Drawing.Size(224, 32);
+            this.RS.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(87, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 26);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Ket Qua";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 26);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Nhap B";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 26);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Nhap A";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(166, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 26);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Giai Phuong Trinh Bac 1";
+            // 
+            // A
+            // 
+            this.A.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A.Location = new System.Drawing.Point(189, 167);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(225, 32);
+            this.A.TabIndex = 17;
+            // 
+            // B
+            // 
+            this.B.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.B.Location = new System.Drawing.Point(189, 209);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(225, 32);
+            this.B.TabIndex = 16;
+            // 
+            // GiaiPTB1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 389);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.B);
+            this.Name = "GiaiPTB1";
+            this.Text = "GiaiPTB1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox RS;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox A;
+        private System.Windows.Forms.TextBox B;
+    }
+}
---
+++ CS434_Calculator/Menu.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace CS434_Calculator
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGiaiPTB2 = new System.Windows.Forms.Button();
+            this.btnCal = new System.Windows.Forms.Button();
+            this.btnGiaiPTB1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGiaiPTB2
+            // 
+            this.btnGiaiPTB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiaiPTB2.Location = new System.Drawing.Point(109, 197);
+            this.btnGiaiPTB2.Name = "btnGiaiPTB2";
+            this.btnGiaiPTB2.Size = new System.Drawing.Size(272, 60);
+            this.btnGiaiPTB2.TabIndex = 5;
+            this.btnGiaiPTB2.Text = "Giải Phương Trình Bậc 2";
+            this.btnGiaiPTB2.UseVisualStyleBackColor = true;
+            // 
+            // btnCal
+            // 
+            this.btnCal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCal.Location = new System.Drawing.Point(108, 286);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(272, 60);
+            this.btnCal.TabIndex = 4;
+            this.btnCal.Text = "Calculator";
+            this.btnCal.UseVisualStyleBackColor = true;
+            // 
+            // btnGiaiPTB1
+            // 
+            this.btnGiaiPTB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGiaiPTB1.Location = new System.Drawing.Point(108, 104);
+            this.btnGiaiPTB1.Name = "btnGiaiPTB1";
+            this.btnGiaiPTB1.Size = new System.Drawing.Size(272, 60);
+            this.btnGiaiPTB1.TabIndex = 3;
+            this.btnGiaiPTB1.Text = "Giải Phương Trình Bậc 1";
+            this.btnGiaiPTB1.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 450);
+            this.Controls.Add(this.btnGiaiPTB2);
+            this.Controls.Add(this.btnCal);
+            this.Controls.Add(this.btnGiaiPTB1);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGiaiPTB2;
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.Button btnGiaiPTB1;
+    }
+}
