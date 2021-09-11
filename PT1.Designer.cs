60e0ec983f760efb268ca98f4a058770dde3ee2a
LucHa2000 <66839444+LucHa2000@users.noreply.github.com>
2021-09-11 19:29:33 +0700

    Rework

---
+++ PT1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PTbac1_2
+{
+    partial class PT1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.giaTriB = new System.Windows.Forms.TextBox();
+            this.giaTriA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.RS = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // giaTriB
+            // 
+            this.giaTriB.Location = new System.Drawing.Point(147, 151);
+            this.giaTriB.Name = "giaTriB";
+            this.giaTriB.Size = new System.Drawing.Size(81, 22);
+            this.giaTriB.TabIndex = 0;
+            // 
+            // giaTriA
+            // 
+            this.giaTriA.Location = new System.Drawing.Point(147, 109);
+            this.giaTriA.Name = "giaTriA";
+            this.giaTriA.Size = new System.Drawing.Size(81, 22);
+            this.giaTriA.TabIndex = 1;
+            this.giaTriA.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Giai Phuong Trinh Bac 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nhap A";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nhap B";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ket Qua";
+            // 
+            // RS
+            // 
+            this.RS.Location = new System.Drawing.Point(147, 252);
+            this.RS.Name = "RS";
+            this.RS.Size = new System.Drawing.Size(224, 22);
+            this.RS.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(284, 192);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 30);
+            this.Reset.TabIndex = 8;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // PT1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 311);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RS);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.giaTriA);
+            this.Controls.Add(this.giaTriB);
+            this.Name = "PT1";
+            this.Text = "PT1";
+            this.Load += new System.EventHandler(this.PT1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox giaTriB;
+        private System.Windows.Forms.TextBox giaTriA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox RS;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Reset;
+    }
+}
---
+++ PT1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTbac1_2
+{
+    public partial class PT1 : Form
+    {
+        public PT1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PT1_Load(object sender, EventArgs e)
+        {
+
+        }
+            private String TinhToan(double giaTriA, double giaTriB)
+
+            {
+
+                String ketQua = "";
+                if (giaTriA == 0)
+                {
+                    if (giaTriB == 0)
+                    {
+                        ketQua = "phuong trinh vo so nghiem";
+                    }
+                    else
+                    {
+                        ketQua = "phuong trinh vo nghiem";
+                    }
+                }
+                else
+                {
+                    ketQua = -giaTriB / giaTriA + "";
+
+                }
+                return ketQua;
+            }
+            private void button1_Click(object sender, EventArgs e)
+            {
+                Double giaTriNhapVaoA = Convert.ToDouble(giaTriA.Text);
+                Double giaTriNhapVapB = Convert.ToDouble(giaTriA.Text);
+                String nghiem = TinhToan(giaTriNhapVaoA, giaTriNhapVapB);
+                RS.Text = nghiem;
+
+
+
+            }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            giaTriA.Text = "";
+            giaTriB.Text = "";
+        }
+    }
+    }
+
+
